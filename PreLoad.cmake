babec3a28dd6e4e3bd309fd696aecb9cc5105d88
benjif <benjamin@frady.org>
2021-07-22 16:09:28 -0400

    Cleanup CMake files a bit.

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -1,4 +1,6 @@
-if (!WIN32)
+FIND_PROGRAM(NINJA "ninja")
+
+if (NINJA)
     set(CMAKE_GENERATOR "Ninja" CACHE INTERNAL "" FORCE)
 else()
     set(CMAKE_GENERATOR "Unix Makefiles" CACHE INTERNAL "" FORCE)
